0e2ebafc5ae30f2954f55429982053299baa686c
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-22 12:18:50 -0500

    Instalación de dev nodemon

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 
 app.listen(4000, () => {
-  console.log("Servidor corriendo en el puerto 4000");
+  console.log("Servidor corriendo desde el puerto 4000");
 });
