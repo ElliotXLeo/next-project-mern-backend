114973f8b7026117db34ad5d2d68bcabff059fcc
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-22 17:44:46 -0500

    update controller userController validación existingUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,22 @@
 import User from "../models/User.js";
 
 export const userCreate = async (req, res) => {
-  try {
-    const user = User(req.body)
-    const saveUser = await user.save();
-    res.json(saveUser);
-    console.log(saveUser);
-  } catch (error) {
-    console.log(error.message);
+  const { email } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    const error = new Error('Usuario ya registrado');
+    return res.status(400).json({
+      message: error.message
+    });
+  } else {
+    try {
+      const user = User(req.body);
+      const saveUser = await user.save();
+      res.json(saveUser);
+      console.log(saveUser);
+    } catch (error) {
+      console.log(error.message);
+    }
   }
-  
 };
