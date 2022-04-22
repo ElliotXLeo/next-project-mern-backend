0ce6eb82a56a6d94eadde8e749884f34f8a8cb57
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-22 17:59:00 -0500

    Update model User hash password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema(
   {
@@ -30,6 +31,15 @@ const userSchema = mongoose.Schema(
     timestamps: true
   }
 );
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  } else {
+    next();
+  }
+});
 
 const User = mongoose.model('user', userSchema);
 export default User;
