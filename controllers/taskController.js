623443430ff1ac08ad30ff720e2ed6e919f53723
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-27 18:27:02 -0500

    controller taskController

---
+++ controllers/taskController.js
@@ -0,0 +1,11 @@
+export const createTask = async (req, res) => { };
+
+export const readTasks = async (req, res) => { };
+
+export const readTask = async (req, res) => { };
+
+export const updateTask = async (req, res) => { };
+
+export const deleteTask = async (req, res) => { };
+
+export const changeTaskStatus = async (req, res) => { };
