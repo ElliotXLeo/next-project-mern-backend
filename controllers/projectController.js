fe55fabc1bfd6003c8868077e4d0aab6a0668c45
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-27 16:42:51 -0500

    Update controller projectController createProject

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,4 +1,16 @@
-export const createProject = async (req, res) => { };
+import Project from "../models/Project.js";
+
+export const createProject = async (req, res) => {
+  const project = new Project(req.body);
+  project.owner = req.user._id;
+  try {
+    const savedProject = await project.save();
+    res.json(savedProject);
+    console.log(savedProject);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const readProjects = async (req, res) => { };
 
